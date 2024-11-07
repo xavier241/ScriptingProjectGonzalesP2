9db86f809037087b3ceb93660f4557ad204e44ae
xavier241 <Xaviergonzales009@gmail.com>
2024-11-07 09:04:42 -0800

    Finish Awake and start
    Finished awake and startt

---
+++ ScriptingProjectP2Gonzales/Assets/Scripts/Awake and Start.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
