55c86715a89a26cc11a4edb456038905e7871b1a
xavier241 <Xaviergonzales009@gmail.com>
2025-01-07 08:59:30 -0800

    finished enabling
    nice

---
+++ ScriptingProjectP2Gonzales/Assets/Scripts/EnablingComponet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablingComponet : MonoBehaviour
+{
+
+    private Light mylight;
+    // Start is called before the first frame update
+    void Start()
+        
+    
+    {
+        mylight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        { 
+            mylight.enabled = !mylight.enabled; 
+        
+        }
+        
+    }
+}
